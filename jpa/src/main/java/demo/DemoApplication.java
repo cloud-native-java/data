92bce14f33c20463c3939adabf7859c816d7cbc2
Kenny Bastani <kb@socialmoon.com>
2015-09-01 01:06:15 -0700

    Fixing typo

--- jpa/src/main/java/demo/DemoApplication.java
+++ jpa/src/main/java/demo/DemoApplication.java
@@ -1,7 +1,5 @@
 package demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,12 +10,6 @@ import java.util.HashSet;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
-
-    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
-
-    @Autowired
-    AccountRepository accountRepository;
-
     @Autowired
     CustomerRepository customerRepository;
 
@@ -40,7 +32,7 @@ public class DemoApplication implements CommandLineRunner {
         // Create a new customer for the account
         Customer customer = new Customer(account, "Jane", "Doe", "jane.doe@gmail.com");
 
-        // Create a new shipping addresses for the customer
+        // Create a new shipping address for the customer
         Address address = new Address(customer, "shipping", "1600 Pennsylvania Ave NW",
                 null, "DC", "Washington", 20500, "United States");
 
@@ -48,6 +40,6 @@ public class DemoApplication implements CommandLineRunner {
         customer.setAddresses(new HashSet<>(Arrays.asList(address)));
 
         // Save the customer object, cascading saves to the object graph
-        log.info(String.valueOf(customerRepository.save(customer)));
+        customerRepository.save(customer);
     }
 }
