3cc9d972f7ce817ef4b73890b7e68518e23785be
Kenny Bastani <kb@socialmoon.com>
2016-01-07 03:35:21 -0500

    Resolving EC2 instance tear down

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,3 @@
 set -e
 
 source $BUILD_DIRECTORY/utils/cf-common.sh
-
-# Create an AWS for docker compost
-sh $BUILD_DIRECTORY/docker-aws.sh create
-
-# Source the environment variables for the integration testing containers
-source $BUILD_DIRECTORY/utils/docker-aws.sh
