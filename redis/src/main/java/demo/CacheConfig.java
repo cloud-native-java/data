3af1ea209a79ff3ef282a835db770ca46877c152
Kenny Bastani <kb@socialmoon.com>
2016-02-09 00:35:21 -0500

    Update Redis section example

--- redis/src/main/java/demo/CacheConfig.java
+++ redis/src/main/java/demo/CacheConfig.java
@@ -2,7 +2,6 @@ package demo;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +12,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 @EnableCaching
-public class CacheConfig extends CachingConfigurerSupport {
+public class CacheConfig {
 
     @Bean
     public JedisConnectionFactory redisConnectionFactory(
